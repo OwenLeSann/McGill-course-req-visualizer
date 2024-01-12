1a8d2fb46a637f3e74930dccc6ece69ce0bdcef0
Owen Le Sann <123684352+OwenLeSann@users.noreply.github.com>
2024-01-12 01:45:36 -0500

    Deleted extra mcgill directory
    This directory was a clone left over after directory restricting in previous commit.

--- mcgill/mcgill/spiders/__init__.py
+++
@@ -1,8 +0,0 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-
-""" The McgillSpider is a Scrapy Crawlspider that scrapes the course webpages listed on the McGill eCalendar.
-The scraped data is then parsed and stored in the fields listed in the items.py file.
-"""
